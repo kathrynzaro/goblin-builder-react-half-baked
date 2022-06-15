446dea2539e68692f8073d833e18c712d6b8a397
kathrynzaro <zarokathryn@gmail.com>
2022-06-15 16:10:00 -0700

    adding functionality

--- src/GoblinList.js
+++ src/GoblinList.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Goblin from './Goblin';
 
-export default function GoblinList(props) {
+export default function GoblinList({ visibleGoblins, handleDeleteGoblin }) {
   return (
     <div className='goblin-list quarter'>
+      {
+        visibleGoblins.map((goblin, i) => <Goblin
+          handleDeleteGoblin={handleDeleteGoblin}
+          key={goblin.name + i} goblin={goblin} />
+        )
+      }
       {/* map over your goblins and render out a Goblin component for each goblin. You've seen this before. The only difference here is that you need to pass handleDeleteGoblin (a prop that is a function), as well */}
     </div>
   );
